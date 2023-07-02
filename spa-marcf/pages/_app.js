dda013ef2adb8d8be648a04d50f85fa2f20f572e
Guillermo L <Guillermo.lucia94@gmail.com>
2023-07-02 21:20:49 +0200

    configuracion basica idiomas

---
+++ spa-marcf/pages/_app.js
@@ -0,0 +1,8 @@
+import '@/styles/globals.css'
+import { appWithTranslation } from 'next-i18next'
+
+const App = ({ Component, pageProps }) => (
+    <Component {...pageProps} />
+)
+
+export default appWithTranslation(App)
