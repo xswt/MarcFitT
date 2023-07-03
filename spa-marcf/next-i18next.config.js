e159c4c7e5ebc2c42c685706cb257f7ed31642c6
Guillermo L <Guillermo.lucia94@gmail.com>
2023-07-03 13:08:04 +0200

    control i18n

--- spa-marcf/next-i18next.config.js
+++ spa-marcf/next-i18next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
     i18n: {
       // all the locales supported in the application
-      locales: ['en', 'it', 'es'], 
+      locales: ['en', 'es'], 
       // the default locale to be used when visiting
       // a non-localized route (e.g. `/about`)   
       defaultLocale: 'en'
